160a12c56cc8eeeda302b2f9ffa781e93d7988d0
Trevor Dixon <trevordixon@gmail.com>
2013-09-20 20:09:36 -0600

    Remove bad semicolon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = angular.module('Search', []);
 app.controller('SearchCtrl', function($scope, $http) {
     $scope.data = [];
 
-    $http.get('https://spreadsheets.google.com/feeds/cells/0AjBVHYwKbJpzdDdWQUhZc3NLNlg4UXllM3doa2tINGc/od6/public/basic?alt=json');
+    $http.get('https://spreadsheets.google.com/feeds/cells/0AjBVHYwKbJpzdDdWQUhZc3NLNlg4UXllM3doa2tINGc/od6/public/basic?alt=json')
     .success(function(response) {
         $scope.data = response.feed.entry.reduce(function(memo, cell, i) {
             var row = ~~(i/3),
